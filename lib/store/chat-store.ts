9a921e7b132139929178a10f50d7f3bb8904dd89
Engr Shegez <adelekeelijah24@gmail.com>
2026-04-03 20:17:35 +0100

    feat: upgraded Zustand store

--- lib/store/chat-store.ts
+++ lib/store/chat-store.ts
@@ -8,11 +8,24 @@ type Message = {
 type ChatStore = {
   messages: Message[];
   addMessage: (msg: Message) => void;
+  updateLastMessage: (content: string) => void;
   setMessages: (msgs: Message[]) => void;
 };
 
 export const useChatStore = create<ChatStore>((set) => ({
   messages: [],
-  addMessage: (msg) => set((state) => ({ messages: [...state.messages, msg] })),
+
+  addMessage: (msg) =>
+    set((state) => ({
+      messages: [...state.messages, msg],
+    })),
+
+  updateLastMessage: (content) =>
+    set((state) => {
+      const updated = [...state.messages];
+      updated[updated.length - 1].content = content;
+      return { messages: updated };
+    }),
+
   setMessages: (msgs) => set({ messages: msgs }),
 }));
