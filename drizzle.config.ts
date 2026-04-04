316c105c72ed65234ba747ca00e08cdd2fbcec88
Engr Shegez <adelekeelijah24@gmail.com>
2026-04-04 17:04:45 +0100

    feat: added drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "tailwind-merge";
+
+export default {
+  schema: "./lib/db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
